55dd5f251d15ff566fdcfd7819d3c25fe96eabac
Konstantin Mamaev <mamaev@magora-systems.com>
2016-09-01 10:42:56 +0700

    Remove content-type header. Update pod version to 0.0.10.

--- NOAPIController/NOAPIController.m
+++ NOAPIController/NOAPIController.m
@@ -7,7 +7,7 @@
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
+//  in the Software without restriction, including without lifmitation the rights
 //  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 //  copies of the Software, and to permit persons to whom the Software is
 //  furnished to do so, subject to the following conditions:
@@ -84,7 +84,6 @@
         request.HTTPBody = bodyData;
 
         [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
-        [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
 
         AFHTTPRequestOperation *operation = [self.requestManager HTTPRequestOperationWithRequest:request
             success:^(AFHTTPRequestOperation *operation, id rawObject) {
@@ -155,7 +154,6 @@
         request.HTTPBody = postData;
 
         [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
-        [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
 
         AFHTTPRequestOperation *operation = [self.requestManager HTTPRequestOperationWithRequest:request
             success:^(AFHTTPRequestOperation *operation, id rawObject) {
